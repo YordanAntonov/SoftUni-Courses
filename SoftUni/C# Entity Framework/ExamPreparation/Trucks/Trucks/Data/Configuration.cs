7924b9b9152f07244e3cfd21bddc5de62850e8af
Yordan Antonov <99537581+YordanAntonov@users.noreply.github.com>
2023-03-30 22:34:03 +0300

    Update Configuration.cs

--- SoftUni/C# Entity Framework/ExamPreparation/Trucks/Trucks/Data/Configuration.cs
+++ SoftUni/C# Entity Framework/ExamPreparation/Trucks/Trucks/Data/Configuration.cs
@@ -2,6 +2,6 @@
 {
     public static class Configuration
     {
-        public static string ConnectionString = @"Server=.;Database=Trucks;User Id=sa;Password=Beatrad1!;MultipleActiveResultSets=true;Trust Server Certificate=true";
+        public static string ConnectionString = @"";
     }
 }
